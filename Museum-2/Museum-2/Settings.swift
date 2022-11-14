3504171d3ad23b005b924bb17fda720656f91aba
Nergaaaal <110521299+Nergaaaal@users.noreply.github.com>
2022-11-14 23:01:38 +0600

    Add files via upload

---
+++ Museum-2/Museum-2/Settings.swift
@@ -0,0 +1,22 @@
+//
+//  Settings.swift
+//  Museum-2
+//
+//  Created by Nurbol on 17.10.2022.
+//
+
+import UIKit
+
+class Settings: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    
+
+    
+
+}
---
+++ Museum-2/Museum-2Tests/Museum_2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Museum_2Tests.swift
+//  Museum-2Tests
+//
+//  Created by Nurbol on 12.10.2022.
+//
+
+import XCTest
+@testable import Museum_2
+
+final class Museum_2Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
